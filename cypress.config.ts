92e460689c43826199741ea068d41185d3e2e941
Samuel Preston <samp.reston@outlook.com>
2022-06-16 14:40:04 +0100

    disable videso

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,11 +6,13 @@ export default defineConfig({
       framework: "create-react-app",
       bundler: "webpack",
     },
-    specPattern: "**/*.cy.{js,jsx,ts,tsx}"
+    specPattern: "**/*.cy.{js,jsx,ts,tsx}",
+    video: false
   },
 
   e2e: {
     baseUrl: 'http://localhost:3000/algo-visualiser',
+    video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
