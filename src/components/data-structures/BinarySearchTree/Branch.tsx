7606e5329ac8504d1a09d3fd5742d867890aa1dd
Samuel Preston <samp.reston@outlook.com>
2022-06-26 02:18:16 +0100

    Improved CSS and added Geneate and Reset buttons

--- src/components/data-structures/BinarySearchTree/Branch.tsx
+++ src/components/data-structures/BinarySearchTree/Branch.tsx
@@ -9,7 +9,7 @@ function Branch(props?: BranchProps) {
   if (!props) return <></>
   return (
     <svg className="branch">
-      <line x1={props.x1} y1={props.y1} x2={props.x2} y2={props.y2} stroke="#CD3F3E" stroke-width="4px" />
+      <line x1={props.x1} y1={props.y1} x2={props.x2} y2={props.y2} stroke="#CD3F3E" strokeWidth="4px" />
     </svg>
   )
 }
